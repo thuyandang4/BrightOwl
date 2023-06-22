49fced8f7b5ece8402e8c347ea145cf818e4697f
Shreyas0610 <arige.shreyas@gmail.com>
2023-06-22 17:10:57 -0500

    Updated Dart SDK and ADDED SIGNUP PAGE

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@
 //   }
 // }
 
-
+import 'package:brightowl_frontend/signup.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,  // removes debug banner
+      debugShowCheckedModeBanner: false, // removes debug banner
       theme: ThemeData(
         useMaterial3: true,
       ),
@@ -52,145 +52,141 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       body: Container(
         height: MediaQuery.of(context).size.height,
-        decoration: BoxDecoration (
+        decoration: BoxDecoration(
           image: DecorationImage(
-              image: const NetworkImage(
-                'https://www.nocontractvoip.com/wp-content/uploads/2023/01/8-Tech-Checks-to-Make-Before-You-Travel-scaled-1.jpg'
-              ),
-              fit: BoxFit.cover,
-              colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.darken),
+            image: const NetworkImage(
+                'https://www.nocontractvoip.com/wp-content/uploads/2023/01/8-Tech-Checks-to-Make-Before-You-Travel-scaled-1.jpg'),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+                Colors.black.withOpacity(0.5), BlendMode.darken),
           ),
         ),
-        child: Center (
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 5.0),
-              child: const Image(
-                image: AssetImage(
-                  'assets/ollieClear.png',                  
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 5.0),
+                child: const Image(
+                  image: AssetImage(
+                    'assets/ollieClear.png',
                   ),
                   width: 250,
                   height: 250,
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(5.0, 10.0, 5.0, 5.0),
-              color: Colors.transparent,
-              child: const Text (
-              "Hi, I'm Ollie!",
-              style: TextStyle(
-                fontSize: 24.0,
-                fontFamily: 'Inter',
-                color: Colors.white,
-              ),
-            )
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(30.0, 10.0, 30.0, 10.0),
-              color: Colors.transparent,
-              child: Text (
-              'Welcome to Bright Owl!',
-              style: GoogleFonts.inter(
-                textStyle: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 55.0,
-                  letterSpacing: 1.2,
-                  height: 1.1,
                 ),
               ),
-              textAlign: TextAlign.center,             
-            )
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(80.0, 30.0, 80.0, 10.0),
-              color: Colors.transparent,
-              child: const Text (
-              'Receive 24/7 tutoring anytime, anywhere.',
-                style: TextStyle (
-                fontSize: 24.0,
-                fontFamily: 'Inter',
-                color: Colors.white,
-                ),
-              textAlign: TextAlign.center,   
-            ),
-            ), 
-            Container(
-              padding: const EdgeInsets.fromLTRB(50.0, 60.0, 50.0, 25.0),
-              color: Colors.transparent,
-              child: Center(
-              child: ElevatedButton (
-                onPressed: () {},
-                style: ElevatedButton.styleFrom (
-                  backgroundColor: Colors.black,
-                  padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-                ),
-                child: Row (
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text (
-                    'I want to learn',
-                     style: TextStyle( 
-                     fontFamily: 'Inter',
-                     fontSize: 22.0,
-                     color: Colors.white
-                    ),
-                  textAlign: TextAlign.center,                  
-                 ),
-                 SizedBox(width: 13),
-                  Image.asset (
-                    alignment: Alignment.center,
-                   'assets/studenticon2.png',
-                    width: 36.0,
-                    height: 30.0,
-                    fit: BoxFit.contain
-                    )
-                  ],
-                ) 
-              )
-              )
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(50.0, 5.0, 50.0, 25.0),
-              color: const Color.fromRGBO(0, 0, 0, 0),
-              child: Center (
-              child: ElevatedButton (
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.black,
-                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-                ),  
-                child: Row (   
-                  mainAxisAlignment: MainAxisAlignment.center,           
-                  children: [
-                  const Text (
-                    'I want to teach  ',
+              Container(
+                  padding: const EdgeInsets.fromLTRB(5.0, 10.0, 5.0, 5.0),
+                  color: Colors.transparent,
+                  child: const Text(
+                    "Hi, I'm Ollie!",
                     style: TextStyle(
+                      fontSize: 24.0,
                       fontFamily: 'Inter',
-                      fontSize: 22.0,
-                      color: Colors.white
+                      color: Colors.white,
+                    ),
+                  )),
+              Container(
+                  padding: const EdgeInsets.fromLTRB(30.0, 10.0, 30.0, 10.0),
+                  color: Colors.transparent,
+                  child: Text(
+                    'Welcome to Bright Owl!',
+                    style: GoogleFonts.inter(
+                      textStyle: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 55.0,
+                        letterSpacing: 1.2,
+                        height: 1.1,
+                      ),
                     ),
                     textAlign: TextAlign.center,
+                  )),
+              Container(
+                padding: const EdgeInsets.fromLTRB(80.0, 30.0, 80.0, 10.0),
+                color: Colors.transparent,
+                child: const Text(
+                  'Receive 24/7 tutoring anytime, anywhere.',
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontFamily: 'Inter',
+                    color: Colors.white,
                   ),
-                  SizedBox(width: 4),
-                  Image.asset (
-                    alignment: Alignment.center,
-                   'assets/teacher.png',
-                    width: 37.0,
-                    height: 30.0,
-                    fit: BoxFit.contain
-                    )
-                  ]
-                )
+                  textAlign: TextAlign.center,
+                ),
               ),
-              )
-            ),
-          ],
+              Container(
+                  padding: const EdgeInsets.fromLTRB(50.0, 60.0, 50.0, 25.0),
+                  color: Colors.transparent,
+                  child: Center(
+                      child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.black,
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 40, vertical: 20),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text(
+                                'I want to learn',
+                                style: TextStyle(
+                                    fontFamily: 'Inter',
+                                    fontSize: 22.0,
+                                    color: Colors.white),
+                                textAlign: TextAlign.center,
+                              ),
+                              SizedBox(width: 13),
+                              Image.asset(
+                                  alignment: Alignment.center,
+                                  'assets/studenticon2.png',
+                                  width: 36.0,
+                                  height: 30.0,
+                                  fit: BoxFit.contain)
+                            ],
+                          )))),
+              Container(
+                  padding: const EdgeInsets.fromLTRB(50.0, 5.0, 50.0, 25.0),
+                  color: const Color.fromRGBO(0, 0, 0, 0),
+                  child: Center(
+                    child: ElevatedButton(
+                        onPressed: () => {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const SignupScreen()))
+                            },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.black,
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 40, vertical: 20),
+                        ),
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text(
+                                'I want to teach  ',
+                                style: TextStyle(
+                                    fontFamily: 'Inter',
+                                    fontSize: 22.0,
+                                    color: Colors.white),
+                                textAlign: TextAlign.center,
+                              ),
+                              SizedBox(width: 4),
+                              Image.asset(
+                                  alignment: Alignment.center,
+                                  'assets/teacher.png',
+                                  width: 37.0,
+                                  height: 30.0,
+                                  fit: BoxFit.contain)
+                            ])),
+                  )),
+            ],
+          ),
         ),
       ),
-    ), 
-  );
+    );
   }
 }
---
+++ lib/signup.dart
@@ -0,0 +1,192 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+import 'package:flutter/material.dart';
+
+
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
+
+  @override
+  State<SignupScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  bool? isChecked = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                height: MediaQuery.of(context).size.height / 2.5,
+                decoration: BoxDecoration(
+                  color:
+                      Color(0xFFFBB628).withOpacity(0.8), // opacity set to 80%
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 30.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        'Sign Up',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 35),
+                      ),
+                      SizedBox(height: 20), // Increased the spacing here
+                      Text(
+                        'Enter information to sign up for free now!',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize:
+                                25), // "Welcome Back" color changed to black
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: MediaQuery.of(context).size.height / 3,
+              child: Container(
+                height: MediaQuery.of(context).size.height / 1.5,
+                width: MediaQuery.of(context).size.width,
+                padding: EdgeInsets.all(30),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                ),
+                child: Column(
+                  children: <Widget>[
+                    const Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text('Email'),
+                    ),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        fillColor: Colors.grey[200],
+                        filled: true,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    const Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text('Password'),
+                    ),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        fillColor: Colors.grey[200],
+                        filled: true,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                      ),
+                      obscureText: true,
+                    ),
+                    const SizedBox(height: 10),
+                    const SizedBox(height: 10),
+                    Container(
+                      width: double.infinity,
+                      height: 50,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'Create Account',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        style: ButtonStyle(
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25.0),
+                            ),
+                          ),
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.black),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Container(
+                        padding:
+                            const EdgeInsets.only(top: 5.0, left: 20.0, right: 20.0),
+                        alignment: Alignment.center,
+                        child: Checkbox(
+                          value: isChecked,
+                          activeColor: Colors.orangeAccent,
+                          //tristate: true,
+                          onChanged: (val) {
+                            print(val);
+                            setState(() {
+                              isChecked = val;
+                            });
+                          },
+                        )),
+                    Container(
+                        child: Column(children: const <Widget>[
+                      /* Checkbox(
+                          value: isChecked,
+                          activeColor: Colors.orangeAccent,
+                          tristate: true,
+                          onChanged: (newBool) {
+                            setState(() {
+                              isChecked = newBool;
+                            });
+                          }),*/
+                      Text(
+                        "By creating an account you are conseting to our ",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 15.0),
+                      ),
+                      Text("Terms and Conditions & Privacy Policy",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 15))
+                    ])),
+                    SizedBox(height: 20),
+                    Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          //Text('Need to make an account? '),
+                          TextButton(
+                            onPressed: () {},
+                            child: const Text(
+                              'Log in?',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  decoration: TextDecoration.underline,
+                                  fontSize: 15),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
